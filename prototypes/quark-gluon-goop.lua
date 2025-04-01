374c8db56f1fb73606b6a85bae9eaaa0521deb11
Tsumiki Miniwa <37684627+cpaca@users.noreply.github.com>
2025-03-31 17:54:47 -0700

    feat: Add quark-gluon goop
    untested, obviously. That's why this is in a branch and not in main.

---
+++ prototypes/quark-gluon-goop.lua
@@ -0,0 +1,11 @@
+-- Item
+data:extend({
+    type = "item",
+    name = "quark-gluon-goop-1mg",
+    icon = "__einstein-factory__/graphics/quark-gluon-goop/icon.png",
+    subgroup = "intermediate-product",
+    stack_size = 1000000, -- 1 million, aka 1KG
+})
+-- Recipe
+
+-- Technology: Not here, will be part of the Einstein Factory tech
