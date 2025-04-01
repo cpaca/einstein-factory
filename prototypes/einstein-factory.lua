c4783c805b1fa72dd5b56fe16cc981ee8eaa0e2b
Tsumiki Miniwa <37684627+cpaca@users.noreply.github.com>
2025-03-31 20:57:33 -0700

    Index off of data.raw, not data.

--- prototypes/einstein-factory.lua
+++ prototypes/einstein-factory.lua
@@ -1,64 +1,69 @@
 -- TODO: Actual graphics
-einstein_factory_icon = data["assembling-machine"]["cryogenic-plant"]["icon"]
-einstein_factory_graphics = data["assembling-machine"]["cryogenic-plant"]["graphics_set"]
+einstein_factory_icon = data.raw["assembling-machine"]["cryogenic-plant"]["icon"]
+einstein_factory_graphics = data.raw["assembling-machine"]["cryogenic-plant"]["graphics_set"]
 
 -- Item
 data:extend({
-    type = "item",
-    name = "einstein-factory",
-    icon = einstein_factory_icon,
-    icon_size = 64,
-    subgroup = "production-machine",
-    order = "c[einstein-factory]",
-    place_result = "einstein-factory",
-    stack_size = 10,
-    weight = 1000000, -- 1 full rocket. i might pick something else later. it's 90GW though so.
+    {
+        type = "item",
+        name = "einstein-factory",
+        icon = einstein_factory_icon,
+        icon_size = 64,
+        subgroup = "production-machine",
+        order = "c[einstein-factory]",
+        place_result = "einstein-factory",
+        stack_size = 10,
+        weight = 1000000, -- 1 full rocket. i might pick something else later. it's 90GW though so.
+    }
 })
 -- Recipe
 data:extend({
-    type = "recipe",
-    name = "einstein-factory",
-    enabled = false,
-    energy_required = 60,
-    ingredients = {
-        {type = "item", name = "tungsten-plate", amount = 250},
-        {type = "item", name = "tungsten-carbide", amount = 100},
-        {type = "item", name = "carbon-fiber", amount = 150},
-        {type = "item", name = "superconductor", amount = 250},
-        {type = "item", name = "quantum-processor", amount = 500},
-        {type = "item", name = "promethium-asteroid", amount = 100},
-        {type = "fluid", name = "fluoroketone-cold", amount = 1000},
-    },
-    results = {{type = "item", name = "einstein-factory", amount = 1}}
+    {
+        type = "recipe",
+        name = "einstein-factory",
+        enabled = false,
+        energy_required = 60,
+        ingredients = {
+            {type = "item", name = "tungsten-plate", amount = 250},
+            {type = "item", name = "tungsten-carbide", amount = 100},
+            {type = "item", name = "carbon-fiber", amount = 150},
+            {type = "item", name = "superconductor", amount = 250},
+            {type = "item", name = "quantum-processor", amount = 500},
+            {type = "item", name = "promethium-asteroid", amount = 100},
+            {type = "fluid", name = "fluoroketone-cold", amount = 1000},
+        },
+        results = {{type = "item", name = "einstein-factory", amount = 1}}
+    }
 })
 
 -- Structure
 data:extend({
-    type = "assembling-machine",
-    name = "einstein-factory",
-    icon = einstein_factory_icon, -- TODO: get the quantum stabilizer icon
-    icon_size = 64,
-    flags = {"placeable-neutral", "placeable-player"},
-    fast_replaceable_group = "einstein-factory",
-    max_health = 500,
-    -- TODO: Circuit stuff?
-    collision_box = {{-2.8, -2.8}, {2.8, 2.8}},
-    selection_box = {{-3.0, -3.0}, {3.0, 3.0}},
-    heating_energy = "1MW",
-    effect_receiver = {uses_module_effects = false, uses_beacon_effects = false, uses_surface_effects = false},
-    module_slots = 0,
-    allowed_effects = {},
-    crafting_categories = {"einstein-factory"},
-    crafting_speed = 1,
-    energy_source = {
-        type = "electric",
-        usage_priority = "secondary-input",
-        emissions_per_minut = {} -- I mean emissions don't really make sense, what would be emitting? CO2?
-    },
-    graphics_set = einstein_factory_graphics,
-    fluid_boxes = {}, -- # no fluid input or output expected for this one.
-    -- fluid_boxes_off_when_no_fluid_recipe = false,
-    
+    {
+        type = "assembling-machine",
+        name = "einstein-factory",
+        icon = einstein_factory_icon, -- TODO: get the quantum stabilizer icon
+        icon_size = 64,
+        flags = {"placeable-neutral", "placeable-player"},
+        fast_replaceable_group = "einstein-factory",
+        max_health = 500,
+        -- TODO: Circuit stuff?
+        collision_box = {{-2.8, -2.8}, {2.8, 2.8}},
+        selection_box = {{-3.0, -3.0}, {3.0, 3.0}},
+        heating_energy = "1MW",
+        effect_receiver = {uses_module_effects = false, uses_beacon_effects = false, uses_surface_effects = false},
+        module_slots = 0,
+        allowed_effects = {},
+        crafting_categories = {"einstein-factory"},
+        crafting_speed = 1,
+        energy_source = {
+            type = "electric",
+            usage_priority = "secondary-input",
+            emissions_per_minut = {} -- I mean emissions don't really make sense, what would be emitting? CO2?
+        },
+        graphics_set = einstein_factory_graphics,
+        fluid_boxes = {}, -- # no fluid input or output expected for this one.
+        -- fluid_boxes_off_when_no_fluid_recipe = false,
+    }
 })
 -- TODO: Technology
 -- TODO: Add the recipes for each "manufactured" item.
@@ -73,19 +78,23 @@ tech_price = {
 }
 
 data:extend({
-    type = "technology",
-    name = "einstein-factory",
-    icon = "__einstein-factory__/graphics/quark-gluon-goop/icon.png", -- TODO: Use the quantum stabilizer's icon. Probably the big version.
-    icon_size = 64,
-    effects = {
-        {type = "unlock-recipe", recipe = "einstein-factory"},
-        {type = "unlock-recipe", recipe = "quark-gluon-goop-1mg"},
-    },
-    prerequisites = {prereq_tech_name},
-    unit = tech_price
+    {
+        type = "technology",
+        name = "einstein-factory",
+        icon = "__einstein-factory__/graphics/quark-gluon-goop/icon.png", -- TODO: Use the quantum stabilizer's icon. Probably the big version.
+        icon_size = 64,
+        effects = {
+            {type = "unlock-recipe", recipe = "einstein-factory"},
+            {type = "unlock-recipe", recipe = "quark-gluon-goop-1mg"},
+        },
+        prerequisites = {prereq_tech_name},
+        unit = tech_price
+    }
 })
 -- TODO: Recipe category
 data:extend({
-    type = "recipe-category",
-    name = "einstein-factory"
+    {
+        type = "recipe-category",
+        name = "einstein-factory"
+    }
 })
