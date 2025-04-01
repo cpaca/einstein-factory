5249e50102b8ec50a5973a229c85cfe6f1c01939
Tsumiki Miniwa <37684627+cpaca@users.noreply.github.com>
2025-03-31 23:34:28 -0700

    Add shadow to einstein-factory

--- prototypes/einstein-factory-graphics.lua
+++ prototypes/einstein-factory-graphics.lua
@@ -10,22 +10,34 @@ out["icon_size"] = 64
 out["graphics_set"] = {
     animation = {
         -- This is an Animation4Way, but that is either a struct of Animations or - in this case - just an Animation.
-        stripes = { -- Array of stripes
-            { -- Stripe: Frames 1-64
-                filename = "__einstein-factory__/graphics/einstein-factory/quantum-stabilizer-hr-animation-1.png",
-                width_in_frames = 8,
-                height_in_frames = 8
+        layers = {
+            -- Layers: Main factory, factory's shadow.
+            { -- Layer: Main factory
+                stripes = { -- Array of stripes
+                    { -- Stripe: Frames 1-64
+                        filename = "__einstein-factory__/graphics/einstein-factory/quantum-stabilizer-hr-animation-1.png",
+                        width_in_frames = 8,
+                        height_in_frames = 8
+                    },
+                    { -- Stripe: Frames 65-100 
+                        filename = "__einstein-factory__/graphics/einstein-factory/quantum-stabilizer-hr-animation-2.png",
+                        width_in_frames = 8,
+                        height_in_frames = 5
+                    }
+                },
+                frame_count = 100,
+                width = 410,
+                height = 410,
+                scale = 0.5
             },
-            { -- Stripe: Frames 65-100 
-                filename = "__einstein-factory__/graphics/einstein-factory/quantum-stabilizer-hr-animation-2.png",
-                width_in_frames = 8,
-                height_in_frames = 5
+            { -- Layer: Factory's shadow
+                filename = "__einstein-factory__/graphics/einstein-factory/quantum-stabilizer-hr-shadow.png",
+                width = 900,
+                height = 420,
+                scale = 0.5,
+                draw_as_shadow = true
             }
-        },
-        frame_count = 100,
-        width = 410,
-        height = 410,
-        scale = 0.5
+        }
     },
     working_visualisations = {
         -- Array of working visualizations
